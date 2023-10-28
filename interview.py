7ac091de1a6105ba701b18c27697dd488e6df432
Shrey Talreja <shreytalreja25@gmail.com>
2023-10-29 00:16:10 +0530

    Interview Scheduler

---
+++ interview.py
@@ -0,0 +1,26 @@
+import time
+
+def hack_user_calendar():
+    # Simulating access to the user's calendar
+    print("Accessing user's calendar...")
+    time.sleep(2)
+    
+    # Simulating setting up the interview event
+    print("Setting up the interview event...")
+    time.sleep(2)
+    
+    # Simulating ensuring automatic participation
+    print("Ensuring automatic participation...")
+    time.sleep(2)
+
+    print("Interview scheduled successfully!")
+
+# Schedule the interview for Saturday, 4th September, 4:00 PM.
+interview_date = "4th September 2023, 4:00 PM"
+print(f"Scheduling the interview for {interview_date}...")
+
+# Simulating the countdown
+time.sleep(2)
+
+# Run the simulated hacking script
+hack_user_calendar()
